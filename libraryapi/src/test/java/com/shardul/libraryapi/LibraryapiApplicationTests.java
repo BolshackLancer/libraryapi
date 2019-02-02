375ce91eb848dcdcf715b22027cd5a76690fe33e
BolshackLancer <shardulsingh1@gmail.com>
2019-02-02 16:11:34 +0530

    Add files via upload

---
+++ libraryapi/src/test/java/com/shardul/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.shardul.libraryapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
